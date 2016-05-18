387a024cf21cd82398ae38fd04dde1c5e5fa93a0
Al-Drac <remi.marchal6@gmail.com>
2016-05-18 14:36:07 +0200

    reglage conflit + add bundle FOSJS

--- scc/app/AppKernel.php
+++ scc/app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new SccBundle\SccBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
