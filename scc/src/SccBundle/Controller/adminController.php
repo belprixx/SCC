e4a6f5fe96cd07fa34baeb081e3a4bd2e5b3991e
Pierre <riviera_paradise@hotmail.fr>
2016-05-23 10:40:36 +0200

    ajouts Theo

--- scc/src/SccBundle/Controller/adminController.php
+++ scc/src/SccBundle/Controller/adminController.php
@@ -4,6 +4,14 @@ namespace SccBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use SccBundle\Entity\Jobs;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 
 class adminController extends Controller
 {
@@ -12,6 +20,24 @@ class adminController extends Controller
      */
     public function adminAction()
     {
+        $Jobs = new Jobs();
+        // On crée le FormBuilder grâce a la fonction createForm
+        $form = $this->createFormBuilder($Jobs)
+            ->add('Titre', TextType::class)
+            ->add('Resume',TextareaType::class,array('label'=>'Contenue'))
+            ->add('NbCoach',ChoiceType::class,array('choices' =>
+                array("1" => 1,
+                    "2" => 2,
+                    "3" => 3,
+                    "4" => 4,
+                    "5" => 5),'label' =>'Nombre de Coach'))
+            ->add('id_user', HiddenType::class, array('data' => 'null'))
+            ->add('Duration',NumberType::class,array('label'=>'Durée'))
+            ->add('StartDate',DateType::HTML5_FORMAT,array('label'=>'Date de début'))
+            ->add('EndDate',DateTimeType::class,array('label'=>'Date de Fin'))
+            ->add('Valider', SubmitType::class, array('label' => 'Valider'))
+            ->getForm();
+
         $candidature = $this -> getDoctrine()
             ->getRepository('SccBundle:Register')
             ->findBy(
