fe9fc0d296215f59cf636cbe6c38d110e61dd15e
Pierre <riviera_paradise@hotmail.fr>
2016-05-23 09:32:22 +0200

    modif css

--- scc/web/Forum/cache/sql_819dc160ed61cca154e8c4eced23fc51.php
+++ scc/web/Forum/cache/sql_819dc160ed61cca154e8c4eced23fc51.php
@@ -1,9 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-1463940013
-=======
-1463924259
->>>>>>> a7fd2c917f87bea2cb014f174e187386a266a781
+1463991203
 SELECT m.*, u.user_colour, g.group_colour, g.group_type FROM (phpbb_moderator_cache m) LEFT JOIN phpbb_users u ON (m.user_id = u.user_id) LEFT JOIN phpbb_groups g ON (m.group_id = g.group_id) WHERE m.display_on_index = 1
 6
 a:0:{}
