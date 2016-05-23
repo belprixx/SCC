6f5441fa5eac5df8a74166da9b3aa2d15171e37f
Al-Drac <remi.marchal6@gmail.com>
2016-05-23 09:32:06 +0200

    modif forum

--- scc/web/Forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ scc/web/Forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,13 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-1463955761
-=======
-<<<<<<< HEAD
-1463939631
-=======
-1463921259
->>>>>>> a7fd2c917f87bea2cb014f174e187386a266a781
->>>>>>> 207eff9f1952cf83efab15b0ebdeb1694699db81
+1463987514
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
