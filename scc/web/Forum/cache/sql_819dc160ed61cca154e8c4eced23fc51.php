de30806e09137e56eaa40ecf0f5c017d92836959
Al-Drac <remi.marchal6@gmail.com>
2016-05-23 09:43:28 +0200

    modif forum

--- scc/web/Forum/cache/sql_819dc160ed61cca154e8c4eced23fc51.php
+++ scc/web/Forum/cache/sql_819dc160ed61cca154e8c4eced23fc51.php
@@ -1,9 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-1463991203
-=======
-1463989783
->>>>>>> d5de726d37e3a8b92cada471b263307e7bf0389c
+1463992879
 SELECT m.*, u.user_colour, g.group_colour, g.group_type FROM (phpbb_moderator_cache m) LEFT JOIN phpbb_users u ON (m.user_id = u.user_id) LEFT JOIN phpbb_groups g ON (m.group_id = g.group_id) WHERE m.display_on_index = 1
 6
 a:0:{}
--- scc/web/Forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
+++ scc/web/Forum/cache/sql_efd26735c3bcbef398e23fb85e7ccd99.php
@@ -1,9 +1,5 @@
 <?php exit; ?>
-<<<<<<< HEAD
-1463988203
-=======
-1463987514
->>>>>>> d5de726d37e3a8b92cada471b263307e7bf0389c
+1463989879
 SELECT forum_id FROM phpbb_forums WHERE forum_options & 2 <> 0 LIMIT 1
 6
 a:0:{}
